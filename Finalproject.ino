e11b10519be3a0d4dcf4a3ca8fcde860871d3a2f
my1524 <33070723+my1524@users.noreply.github.com>
2017-11-11 19:53:13 -0500

    Added audio code and framework
    Added the code needed to connect from an adc and use the fft, also created framework for led array

--- Finalproject.ino
+++ Finalproject.ino
@@ -1,11 +1,37 @@
-int ledPin = 20;
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputAnalog         adc1;           //xy=161,106
+AudioAnalyzeFFT256       fft256_1;       //xy=361,110
+AudioConnection          patchCord1(adc1, fft256_1);
+// GUItool: end automatically generated code
+
+//5x5 array representing the 5 rows of 5 leds I'll use
+int npLights[5][5] = {
+  {,,,,},
+  {,,,,},
+  {,,,,},
+  {,,,,},
+  {,,,,}
+};
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+/*connect sine wave to program through crazy amount of resistors, 
+ * print out just if it's loud 
+*/
+//function to analyze audio with fft
+void analyzeAudio(){
 }
